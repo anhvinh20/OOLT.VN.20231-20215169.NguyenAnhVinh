509824c7ef985616dc333f5716342909f7a1d4af
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-10-13 02:00:50 +0700

    vinh

---
+++ Lab1_222.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Lab1_222 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+        System.exit(0);
+    }
+}
---
+++ Lab1_225.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class Lab1_225 {
+    public static void main(String[] args) {
+        double num1 , num2 , num3, num4, num5;
+
+        String strNum1 ,strNum2 ;
+
+        strNum1 = JOptionPane.showInputDialog(null,
+                " Please input the first number: ", " Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+        num1 = Double.parseDouble(strNum1);
+
+
+        strNum2 = JOptionPane.showInputDialog(null,
+                " Please input the second number: ", " Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        num2 = Double.parseDouble(strNum2);
+        num3= num1 +num2 ;
+        num4 = Math.abs(num1-num2);
+        num5 = num1/num2 ;
+//    JOptionPane.showMessageDialog(null, num1 + "+" + num2+ "="+ num3,
+//    		"Show sum two numbers", JOptionPane.INFORMATION_MESSAGE);
+//    System.exit(0);
+
+        JOptionPane.showMessageDialog(null, "|"+num1 + "-" + num2+"|"+ "="+ num4,
+                "Show abs diff two numbers", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
+        JOptionPane.showMessageDialog(null, num1 + "/" + num2 + "="+ num5,
+                "Show abs diff two numbers", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
+    }
+}
