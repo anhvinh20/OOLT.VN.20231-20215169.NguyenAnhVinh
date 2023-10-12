15fedf55089ff49bd33c658f119afdbd3a5cae48
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-10-12 14:41:11 +0700

    vinh

--- b1.java
+++ b1.java
@@ -6,7 +6,7 @@ public class b1 {
 	public static void main(String[] args) {
 		System.out.println("xin chao \n cac ban!");
 		System.out.println("Hello \t world!");
-		
+	     System.out.println("vinh");
 		JOptionPane.showMessageDialog(null, "Hello world! How are you?");
 		System.exit(0);
 	}
