9b54d7c255f84c696d8909502075acece209aa41
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-10-25 21:37:22 +0700

    v

--- LAB1/Lab1_223.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class Lab1_223 {
-    public static void main(String[] args) {
-        String result;
-        result = JOptionPane.showInputDialog("Please enter your name: ");
-        JOptionPane.showMessageDialog(null, "Hi " +result +"!");
-        System.exit(0);
-    }
-}
