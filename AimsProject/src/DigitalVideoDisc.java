269d22e8fb4504d81727146a6af853d3c440c53c
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-10-28 17:45:34 +0700

    .

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -4,7 +4,8 @@ public class DigitalVideoDisc {
     private String director ;
     private int length ;
     private float cost;
-
+    private static int nbDigitalVideoDiscs = 0;
+    private int id ;
 
     public String getTitle() {
         return title;
@@ -32,14 +33,16 @@ public class DigitalVideoDisc {
     // Constructor
     public DigitalVideoDisc(String title) {
         this.title = title;
-
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
-
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -47,7 +50,8 @@ public class DigitalVideoDisc {
         this.category = category;
         this.director = director;
         this.cost = cost;
-
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -56,7 +60,8 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
-
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
     @Override
