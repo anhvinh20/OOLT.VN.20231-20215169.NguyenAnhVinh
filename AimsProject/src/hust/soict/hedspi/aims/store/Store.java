ff5a3d3e234c04d2c571fcf2d159333923e88072
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-11-23 12:48:09 +0700

    update Store and StoreTest

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -2,36 +2,20 @@ package hust.soict.hedspi.aims.store;
 
 import hust.soict.hedspi.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
 
+import java.util.ArrayList;
+
 public class Store {
-    public static final int MAX_NUMBER_STORED = 20;
-    private DigitalVideoDisc itemsInStore[] =
-            new DigitalVideoDisc[MAX_NUMBER_STORED];
-
-    private int qtyStored = 0 ;
-
-    //Adds the DVD
-    public void addDVD(DigitalVideoDisc disc){
-        if(qtyStored <MAX_NUMBER_STORED){
-            itemsInStore[qtyStored] = disc;
-            qtyStored++;
-            System.out.println("The disc has been added in the store.");
-        }else{
-            System.out.println("The store is almost full!");
-        }
+    public Store() {
     }
 
-    //Removes the DVD
-    public void removeDVD(DigitalVideoDisc dics){
-        for (int i = 0; i < qtyStored; i++) {
-            if (itemsInStore[i] != null ) {
-                itemsInStore[i] = null;
-                qtyStored--;
-                System.out.println("The disc in the store has been removed.");
-                return;
-            }
-        }
-        System.out.println("The disc was not found in the store.");
+    ArrayList<DigitalVideoDisc> ItemsInStore = new ArrayList<DigitalVideoDisc>();
+    public void removeDVD(DigitalVideoDisc disc) {
+        ItemsInStore.remove(disc);
+        System.out.println("The media " + disc.getTitle() + " has been removed");
     }
 
-
+    public void addDVD(DigitalVideoDisc disc) {
+        ItemsInStore.add(disc);
+        System.out.println("The disc " + disc.getTitle() + " has been added");
+    }
 }
--- AimsProject/src/hust/soict/hedspi/aims/store/StoreTest.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/StoreTest.java
@@ -3,10 +3,20 @@ package hust.soict.hedspi.aims.store;
 import hust.soict.hedspi.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
 
 public class StoreTest {
-   Store dvd = new Store();
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        store.addDVD(dvd1);
 
-    // Create new dvd objects and add them to the cart
-    DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-            "Animation", "Roger Allers", 87, 19.95f);
-        dvd.addDVD(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction","George Lucas",87,24.95f);
+        store.addDVD(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation",18.99f);
+        store.addDVD(dvd3);
+
+        store.removeDVD(dvd1);
+    }
 }
