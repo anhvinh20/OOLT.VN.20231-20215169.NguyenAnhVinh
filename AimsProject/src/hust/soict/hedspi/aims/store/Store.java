d5597c9d6af2d40b88789fd6198ed8baee1d2fe7
Vinh Nguyễn <106879891+anhvinh20@users.noreply.github.com>
2023-11-30 12:49:30 +0700

    update Aims

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -18,7 +18,33 @@ public class Store {
         }
 
     }
+    public Media searchById(int id) {
+        System.out.println("Search results for ID: " + id);
+        for (Media m : ItemsInStore) {
+            if (m.getId() == id) {
+                System.out.println(m.toString());
+                return m;
+            }
+        }
+        System.out.println("No items found.");
+        return null;
+    }
 
+
+    public Media searchByTitle(String title) {
+        boolean found = false;
+        System.out.println("Search results for keywords: " + title);
+        for (Media m : ItemsInStore) {
+            if (m.isMatch(title)) {
+                System.out.println(m.toString());
+                found = true;
+                return m;
+            }
+        }
+        if (!found)
+            System.out.println("No items found.");
+        return null;
+    }
     public void addMedia(Media media) {
         ItemsInStore.add(media);
         System.out.println("The media " + media.getTitle() + " has been added");
